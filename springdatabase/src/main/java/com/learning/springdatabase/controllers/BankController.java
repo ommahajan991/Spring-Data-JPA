3607791736f747a4fffa518404e66f5fce6a6bf7
Om Mahajan <ommahajan385@gmail.com>
2026-01-26 14:14:27 +0530

    initial commit

--- springdatabase/src/main/java/com/learning/springdatabase/controllers/BankController.java
+++ springdatabase/src/main/java/com/learning/springdatabase/controllers/BankController.java
@@ -2,8 +2,16 @@ package com.learning.springdatabase.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.learning.springdatabase.services.BankService;
+
 @RestController
 public class BankController {
+
+	private BankService bankService;
+
+	public BankController(BankService bankService) {
+		this.bankService = bankService;
+	}
 
 	/** Handler Methods */
 
