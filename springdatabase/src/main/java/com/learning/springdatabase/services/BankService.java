1d17ae7a2acd019bea42849c7842624d45ae8a19
Om Mahajan <ommahajan385@gmail.com>
2026-01-27 13:51:10 +0530

    commit

--- springdatabase/src/main/java/com/learning/springdatabase/services/BankService.java
+++ springdatabase/src/main/java/com/learning/springdatabase/services/BankService.java
@@ -1,7 +1,22 @@
 package com.learning.springdatabase.services;
 
+import java.util.List;
+
+import com.learning.springdatabase.dto.BankDto;
+
 public interface BankService {
 
-	/** Service Methods */
+	/** Abstract Service Methods */
 
+	public BankDto getBank(int id);
+
+	public List<BankDto> getAllBanks();
+
+	public BankDto createBank(BankDto bankDto);
+
+	public BankDto updateBank(int id, BankDto bankDto);
+
+	public void deleteBank(int id);
+	
+	public void deleteAllBanks(int id);
 }
--- springdatabase/src/main/java/com/learning/springdatabase/services/implementations/BankServiceImplementation.java
+++ springdatabase/src/main/java/com/learning/springdatabase/services/implementations/BankServiceImplementation.java
@@ -1,8 +1,11 @@
 package com.learning.springdatabase.services.implementations;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.learning.springdatabase.dto.BankDto;
 import com.learning.springdatabase.repositories.BankRepository;
 import com.learning.springdatabase.services.BankService;
 
@@ -16,4 +19,34 @@ public class BankServiceImplementation implements BankService {
 	private final BankRepository bankRepository;
 
 	/** Overridden Service Methods */
+
+	@Override
+	public BankDto getBank(int id) {
+		return null;
+	}
+
+	@Override
+	public List<BankDto> getAllBanks() {
+		return null;
+	}
+
+	@Override
+	public BankDto createBank(BankDto bankDto) {
+		return null;
+	}
+
+	@Override
+	public BankDto updateBank(int id, BankDto bankDto) {
+		return null;
+	}
+
+	@Override
+	public void deleteBank(int id) {
+
+	}
+
+	@Override
+	public void deleteAllBanks(int id) {
+
+	}
 }
