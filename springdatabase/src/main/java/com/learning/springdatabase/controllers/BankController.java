cbb043d49c7a08d9d90ead779d59ec519a673ded
Om Mahajan <ommahajan385@gmail.com>
2026-01-29 13:22:08 +0530

    controller added

--- springdatabase/src/main/java/com/learning/springdatabase/controllers/BankController.java
+++ springdatabase/src/main/java/com/learning/springdatabase/controllers/BankController.java
@@ -1,7 +1,13 @@
 package com.learning.springdatabase.controllers;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,9 +25,34 @@ public class BankController {
 	}
 
 	/** Handler Methods */
-
-	@GetMapping("/{id")
+	
+	@GetMapping("/{id}")
 	public BankDto getBank(@PathVariable int id) {
 		return bankService.getBank(id);
+	}
+
+	@GetMapping
+	public List<BankDto> getAllBanks() {
+		return bankService.getAllBanks();
+	}
+
+	@PostMapping
+	public BankDto createBank(@RequestBody BankDto bankDto) {
+		return bankService.createBank(bankDto);
+	}
+
+	@PutMapping("/{id}")
+	public BankDto updateBank(@PathVariable int id, @RequestBody BankDto bankDto) {
+		return bankService.updateBank(id, bankDto);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteBank(@PathVariable int id) {
+		bankService.deleteBank(id);
+	}
+	
+	@DeleteMapping
+	public void deleteAllBank() {
+		bankService.deleteAllBanks();
 	}
 }
